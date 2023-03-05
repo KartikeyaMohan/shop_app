3ca44717916b9d34b416c4215d2ba982a9a8e5d7
Kartikeya Mohan <kartikeyamohan@gmail.com>
2023-03-05 17:36:26 +0530

    Removed API key

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -33,7 +33,7 @@ class Auth with ChangeNotifier{
   }
 
   Future<void> _authenticate(String? email, String? password, String urlSegment) async {
-    final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDuB9CCQHco79S4SPBcVU3-dMJzViEPkIc';
+    final url = 'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=[API_KEY]';
     try {
       final response = await http.post(
         Uri.parse(url),
